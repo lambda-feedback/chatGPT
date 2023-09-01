6d0c2d945a73ffb3ef84a7a9f43c32ebf75b29ad
lewismoss <138524447+lewismoss@users.noreply.github.com>
2023-09-01 22:12:37 +0100

    updated 'default_prompt'

--- app/evaluation_tests.py
+++ app/evaluation_tests.py
@@ -7,7 +7,7 @@ except ImportError:
 
 model = 'gpt-3.5-turbo'
 
-default_prompt = "Provide feedback to the student's answer in first person. Output a boolean (True if the student is correct and False if the student is wrong), a vertical bar |, and then your feedback. i.e., your response should be in the form: boolean|feedback"
+default_prompt = "Output a Boolean: True if the student is correct and False if the student is incorrect"
 
 answer = 1
 
@@ -17,7 +17,7 @@ class TestEvaluationFunction(unittest.TestCase):
         response = "The pressurised vessel, because it could explode and cause injury if it's overpressurised."
         parameters = {'model': model, 
                       'main_prompt': "The student needs to enter a risk with a short description of how it can cause harm",
-                      'feedback_prompt': "Provide objective and constructive feedback to the student",
+                      'feedback_prompt': "You are an AI who should provide objective and constructive feedback to the student",
                       'default_prompt': default_prompt}
         output = evaluation_function(response, answer, parameters)
         self.assertEqual(output['is_correct'], True)
@@ -26,7 +26,7 @@ class TestEvaluationFunction(unittest.TestCase):
         response = "Photosynthesis is the process by which plants convert light energy into chemical energy to fuel their growth."
         parameters = {'model': model, 
                       'main_prompt': "Evaluate the student's response for the definition of photosynthesis",
-                      'feedback_prompt': "Provide objective and constructive feedback to the student",
+                      'feedback_prompt': "You are an AI who should provide objective and constructive feedback to the student",
                       'default_prompt': default_prompt}
         output = evaluation_function(response, answer, parameters)
         self.assertEqual(output["is_correct"], True)
@@ -35,7 +35,7 @@ class TestEvaluationFunction(unittest.TestCase):
         response = "Photosynthesis is the process by which plants make their food."
         parameters = {'model': model, 
                       'main_prompt': "Evaluate the student's response for the definition of photosynthesis. They should mention the conversion of light energy to chemical energy.",
-                      'feedback_prompt': "Provide objective and constructive feedback to the student",
+                      'feedback_prompt': "You are an AI who should provide objective and constructive feedback to the student",
                       'default_prompt': default_prompt}
         output = evaluation_function(response, answer, parameters)
         self.assertEqual(output["is_correct"], False)
@@ -44,7 +44,7 @@ class TestEvaluationFunction(unittest.TestCase):
         response = "The capital of France is Berlin."
         parameters = {'model': model, 
                       'main_prompt': "Analyze the response regarding the capital of France",
-                      'feedback_prompt': "Provide objective and constructive feedback to the student",
+                      'feedback_prompt': "You are an AI who should provide objective and constructive feedback to the student",
                       'default_prompt': default_prompt}
         output = evaluation_function(response, answer, parameters)
         self.assertEqual(output["is_correct"], False)
@@ -53,7 +53,7 @@ class TestEvaluationFunction(unittest.TestCase):
         response = "The capital of France is Paris, which is the largest city in the world."
         parameters = {'model': model, 
                       'main_prompt': "Analyze the response regarding the capital of France",
-                      'feedback_prompt': "Provide objective and constructive feedback to the student",
+                      'feedback_prompt': "You are an AI who should provide objective and constructive feedback to the student",
                       'default_prompt': default_prompt}
         output = evaluation_function(response, answer, parameters)
         self.assertEqual(output["is_correct"], False)
@@ -62,7 +62,7 @@ class TestEvaluationFunction(unittest.TestCase):
         response = "Red, blue and yellow."
         parameters = {'model': model, 
                       'main_prompt': "Mark this response asking students for the three primary colours in painting.",
-                      'feedback_prompt': "Provide objective and constructive feedback to the student",
+                      'feedback_prompt': "You are an AI who should provide objective and constructive feedback to the student",
                       'default_prompt': default_prompt}
         output = evaluation_function(response, answer, parameters)
         self.assertEqual(output["is_correct"], True)
@@ -71,7 +71,7 @@ class TestEvaluationFunction(unittest.TestCase):
         response = "The law of conservation of energy states that energy cannot be created or destroyed, only transformed from one form to another. It's a fundamental principle in physics."
         parameters = {'model': model, 
                       'main_prompt': "Examine the explanation of the law of conservation of energy and provide feedback.",
-                      'feedback_prompt': "Provide objective and constructive feedback to the student",
+                      'feedback_prompt': "You are an AI who should provide objective and constructive feedback to the student",
                       'default_prompt': default_prompt}
         output = evaluation_function(response, answer, parameters)
         self.assertEqual(output["is_correct"], True)
